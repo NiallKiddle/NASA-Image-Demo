9035fd922bf6d88112b2f1b82e599edd588ad4a4
Niall Kiddle <ct1400@live.co.uk>
2019-03-16 14:34:54 +0000

    Base layout for HomeViewController with header view animation

---
+++ Milky Way demo/Cells/ImageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionViewCell.swift
+//  Milky Way demo
+//
+//  Created by Niall Kiddle on 16/03/2019.
+//  Copyright © 2019 Intrica Studio. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Milky Way demo/View Controllers/Header view/HeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderView.swift
+//  Milky Way demo
+//
+//  Created by Niall Kiddle on 16/03/2019.
+//  Copyright © 2019 Intrica Studio. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
