c8bc3a5fdf02f5406b01f5a562eb68a4202b6148
Niall Kiddle <ct1400@live.co.uk>
2019-03-22 00:09:05 +0000

    Unit test network response and decode into model

--- Milky Way demoTests/NetworkTests.swift
+++ Milky Way demoTests/NetworkTests.swift
@@ -27,7 +27,7 @@ class NetworkTests: XCTestCase {
     
     // MARK: - API calls
     
-    func callToAPISucceeds()
+    func testCallToAPISucceeds()
     {
         let url = URL(string: "https://images-api.nasa.gov/search?q=milky%20way&media_type=image&year_start=2017&year_end=2017")
         let promise = expectation(description: "Completion handler invoked")
@@ -49,13 +49,47 @@ class NetworkTests: XCTestCase {
         XCTAssertEqual(statusCode, 200)
     }
     
-    func decodeResponseSucceeds()
+    func testDecodeResponse()
     {
         let url = URL(string: "https://images-api.nasa.gov/search?q=milky%20way&media_type=image&year_start=2017&year_end=2017")
+        let promise = expectation(description: "Completion handler invoked")
+        
+        var statusCode: Int?
+        var responseError: Error?
+        
+        var itemModel: ItemModel?
         
         let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
             
+            guard let data = data else { return XCTFail() }
+            
+            // Decode data
+            do {
+                let responseData = try JSONDecoder().decode(ResponseModel.self, from: data)
+                guard let collection = responseData.collection else { return XCTFail() }
+                
+                itemModel = collection.items.first
+                
+            } catch let JSONErr {
+                print("JSON Error: ", JSONErr)
+                return
+            }
+            
+            promise.fulfill()
         }
         dataTask.resume()
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // Ensure data has been correctly decoded into model
+        XCTAssertNotNil(itemModel)
+        XCTAssert(itemModel?.data?.first?.keywords?.count != 0)
+        XCTAssert(itemModel?.data?.first?.title != "")
+        XCTAssert(itemModel?.data?.first?.description != "")
+        
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
     }
 }
